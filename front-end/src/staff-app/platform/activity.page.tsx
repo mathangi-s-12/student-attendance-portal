d34ce91dbd53d4b6f1ecbf747daa77350bd44629
Mathangi S <mathangi98@gmail.com>
2022-10-12 22:28:43 +0530

    feat: add activity page

--- front-end/src/staff-app/platform/activity.page.tsx
+++ front-end/src/staff-app/platform/activity.page.tsx
@@ -1,16 +1,168 @@
-import React from "react"
+// Libs
+import React, { useEffect, useState } from "react"
 import styled from "styled-components"
-import { Spacing } from "shared/styles/styles"
+import { debounce } from "debounce"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+// Styles
+import { Spacing, BorderRadius, FontWeight } from "shared/styles/styles"
+import { Colors } from "shared/styles/colors"
+// Custom Hooks
+import { useApi } from "shared/hooks/use-api"
+// Types and interfaces
+import { Activity } from "shared/models/activity"
+import { RolllStateType, StudentRoll } from "shared/models/roll"
+// Components
+import { SORT_STATES, SortOrders } from "shared/components/sort/sort.component"
+import { CenteredContainer } from "shared/components/centered-container/centered-container.component"
+import Toolbar from "./activity-toolbar"
+import ActivityListTile from "staff-app/components/activity-list-title/activity-list-tile.component"
+// Context
+import { ActivitiesContextType, ActivitiesProvider } from "staff-app/contexts/activity-page-context"
+
+type ActivitiesSortParams = "completed_at" | RolllStateType
+
+type ActivitiesSortParamsObject = {
+  [key: string]: { value: ActivitiesSortParams; label: string }
+}
+
+const ACTIVITIES_SORT_PARAMS: ActivitiesSortParamsObject = {
+  completedAt: { value: "completed_at", label: "Completed at" },
+  late: { value: "late", label: "Late count" },
+  present: { value: "present", label: "Present count" },
+  absent: { value: "absent", label: "Absent count" },
+  unmark: { value: "unmark", label: "Unmark count" },
+}
 
 export const ActivityPage: React.FC = () => {
-  return <S.Container>Activity Page</S.Container>
+  const [sortBy, setSortBy] = useState<ActivitiesSortParams>(ACTIVITIES_SORT_PARAMS.completedAt.value)
+  const [sortOrder, setSortOrder] = useState<SortOrders>(null)
+  const [searchTerm, setSearchTerm] = useState<string>("")
+
+  const [getActivities, data, loadState] = useApi<{ activity: Activity[] }>({ url: "get-activities" })
+
+  useEffect(() => {
+    void getActivities()
+  }, [getActivities])
+
+  const handleSortOrderChange = (order: SortOrders) => {
+    setSortOrder(order)
+  }
+
+  const handleSortByChange = (sortBy: string) => {
+    setSortBy(sortBy as ActivitiesSortParams)
+  }
+
+  const handleSearchTermChange = debounce((searchTerm: string) => {
+    setSearchTerm(searchTerm)
+  }, 300)
+
+  const activitiesContextValue: ActivitiesContextType = {
+    data: data?.activity || [],
+    sort: {
+      sortOptions: Object.values(ACTIVITIES_SORT_PARAMS),
+      sortOrder,
+      sortBy,
+      handleSortOrderChange,
+      handleSortByChange,
+    },
+    search: {
+      searchTerm,
+      handleSearchTermChange,
+    },
+  }
+
+  const getCount = (activity: Activity, type: RolllStateType) => {
+    return activity.entity.student_roll_states.filter((rollState: StudentRoll) => rollState.roll_state === type).length
+  }
+
+  const getSortedData = (rolls: Activity[]) => {
+    if (sortOrder === null) return [...rolls]
+    return [...rolls].sort((a: Activity, b: Activity) => {
+      switch (sortBy) {
+        case "unmark":
+        case "present":
+        case "absent":
+        case "late":
+          const countA = getCount(a, sortBy)
+          const countB = getCount(b, sortBy)
+          return sortOrder === SORT_STATES.asc ? countA - countB : countB - countA
+        case "completed_at":
+          const dateA: Date = new Date(a.entity.completed_at)
+          const dateB: Date = new Date(b.entity.completed_at)
+          return sortOrder === SORT_STATES.asc ? dateA - dateB : dateB - dateA
+        default:
+          return 0
+      }
+    })
+  }
+
+  const getSearchedData = (rolls: Activity[]) => {
+    return [...rolls].filter((roll: Activity) => {
+      const rollName: string = roll.entity.name
+      const searchTerms: string[] = searchTerm
+        .trim()
+        .replace(/\s\s+/g, " ")
+        .split(" ")
+        .map((word: string) => word.toLowerCase())
+      const isSearchTermInName = searchTerms.reduce((acc: boolean, term: string) => acc || rollName.toLowerCase().includes(term), false)
+      return isSearchTermInName
+    })
+  }
+
+  const getSortedAndFilteredData = (activities: Activity[]) => {
+    if (!data?.activity) return []
+    const filteredSearchData = [...getSearchedData(activities)]
+    const sortedData = [...getSortedData(filteredSearchData)]
+
+    return [...sortedData]
+  }
+
+  return (
+    <>
+      <ActivitiesProvider value={activitiesContextValue}>
+        <S.PageContainer>
+          <Toolbar />
+
+          {loadState === "loading" && (
+            <CenteredContainer>
+              <FontAwesomeIcon icon="spinner" size="2x" spin />
+            </CenteredContainer>
+          )}
+
+          {loadState === "loaded" && data?.activity && (
+            <>
+              {getSortedAndFilteredData(data.activity).map((s) => (
+                <ActivityListTile key={s.entity.id} activity={s} />
+              ))}
+            </>
+          )}
+
+          {loadState === "error" && (
+            <CenteredContainer>
+              <div>Failed to load</div>
+            </CenteredContainer>
+          )}
+        </S.PageContainer>
+      </ActivitiesProvider>
+    </>
+  )
 }
 
 const S = {
-  Container: styled.div`
+  PageContainer: styled.div`
     display: flex;
     flex-direction: column;
     width: 50%;
-    margin: ${Spacing.u4} auto 0;
+    margin: ${Spacing.u4} auto 140px;
+  `,
+  ToolbarContainer: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #fff;
+    background-color: ${Colors.blue.base};
+    padding: 10px 14px;
+    font-weight: ${FontWeight.strong};
+    border-radius: ${BorderRadius.default};
   `,
 }
