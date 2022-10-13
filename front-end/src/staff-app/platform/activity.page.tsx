227b3777f5aa6d457a8c61b8d9ab2090c5bae404
Mathangi S <mathangi98@gmail.com>
2022-10-13 23:13:42 +0530

    feat: add a toast to mention functionality of roll state icons in activity page

--- front-end/src/staff-app/platform/activity.page.tsx
+++ front-end/src/staff-app/platform/activity.page.tsx
@@ -12,6 +12,7 @@ import { useApi } from "shared/hooks/use-api"
 import { Activity } from "shared/models/activity"
 import { RolllStateType, StudentRoll } from "shared/models/roll"
 // Components
+import toast from "shared/components/toast/toast"
 import { SORT_STATES, SortOrders } from "shared/components/sort/sort.component"
 import { CenteredContainer } from "shared/components/centered-container/centered-container.component"
 import Toolbar from "./activity-toolbar"
@@ -33,12 +34,21 @@ const ACTIVITIES_SORT_PARAMS: ActivitiesSortParamsObject = {
   unmark: { value: "unmark", label: "Unmark count" },
 }
 
+let isOnboard: boolean = true
+
 export const ActivityPage: React.FC = () => {
   const [sortBy, setSortBy] = useState<ActivitiesSortParams>(ACTIVITIES_SORT_PARAMS.completedAt.value)
   const [sortOrder, setSortOrder] = useState<SortOrders>(null)
   const [searchTerm, setSearchTerm] = useState<string>("")
 
   const [getActivities, data, loadState] = useApi<{ activity: Activity[] }>({ url: "get-activities" })
+
+  useEffect(() => {
+    if (isOnboard) {
+      toast("info", "Click on roll state icons to view students in each roll state")
+      isOnboard = false
+    }
+  }, [])
 
   useEffect(() => {
     void getActivities()
