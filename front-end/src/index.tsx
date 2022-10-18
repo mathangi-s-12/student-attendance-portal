b704bdf5e6a8f26067fb53d3ee3d15bbe5bdc382
Mathangi S <mathangi98@gmail.com>
2022-10-18 16:15:55 +0530

    feat: check deployment with browser router

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import "index.css"
